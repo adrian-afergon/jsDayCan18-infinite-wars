fe217f962e8f94c18450931b67f3d70840b574fb
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-10-21 20:50:33 +0100

    feat(Share): Create tag component.

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TitleComponent} from './components/title/title.component';
-import {SubtitleComponent} from './components/subtitle/subtitle.component';
+import { TitleComponent } from './components/title/title.component';
+import { SubtitleComponent } from './components/subtitle/subtitle.component';
+import { TagComponent } from './components/tag/tag.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TitleComponent, SubtitleComponent],
-  exports: [TitleComponent, SubtitleComponent]
+  declarations: [TitleComponent, SubtitleComponent, TagComponent],
+  exports: [TitleComponent, SubtitleComponent, TagComponent]
 })
 export class ShareModule { }
