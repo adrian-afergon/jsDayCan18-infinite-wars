dffd0521cfe218239a1eefd3f9cfe8d2faf76b2a
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-10-21 18:35:23 +0100

    feat(Footer): create the footer component.

---
+++ src/app/core/components/footer/footer.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterComponent } from './footer.component';
+
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FooterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should have a title', () => {
+    const title = fixture.debugElement.nativeElement.querySelector('h1');
+    expect(title).toBeTruthy();
+  });
+  it('should have a links to social newtorks', () => {
+    const links = fixture.debugElement.nativeElement.querySelectorAll('li');
+    expect(links.length).toBe(5);
+  });
+});
