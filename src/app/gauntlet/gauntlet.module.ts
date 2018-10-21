09e8bba546c78bca5084514ff8c5c628b0a4d5ac
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-10-21 17:17:19 +0100

    feat(Containers): add empty containers.

--- src/app/gauntlet/gauntlet.module.ts
+++ src/app/gauntlet/gauntlet.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GauntletComponent } from './containers/gauntlet/gauntlet.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [GauntletComponent]
 })
 export class GauntletModule { }
--- src/app/population/population.module.ts
+++ src/app/population/population.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PopulationComponent } from './containers/population/population.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [PopulationComponent]
 })
 export class PopulationModule { }
