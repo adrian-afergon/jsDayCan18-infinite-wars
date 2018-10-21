bbbb3f1214cd3b992a3fbbd3114f073fbe431cfc
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-10-21 18:02:12 +0100

    feat(Header): create a header component.

---
+++ src/app/core/components/header/header.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should have a title', () => {
+    const title = fixture.debugElement.nativeElement.querySelector('h1');
+    expect(title).toBeTruthy();
+  });
+  it('should have a links to pages', () => {
+    const list = fixture.debugElement.nativeElement.querySelectorAll('li');
+    expect(list.length).toBe(2);
+  });
+});
