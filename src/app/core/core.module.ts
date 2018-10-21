233f6f6bc94a60e066424c03543719265a56a82e
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-10-21 18:54:23 +0100

    feat(Core): Configure core module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './containers/main/main.component';
+import { CoreRouter } from './app.routing';
+import {RouterModule} from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { TitleComponent } from './components/title/title.component';
+import { SubtitleComponent } from './components/subtitle/subtitle.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    CoreRouter
   ],
-  declarations: []
+  declarations: [HeaderComponent, MainComponent, FooterComponent, TitleComponent, SubtitleComponent],
+  exports: [MainComponent]
 })
 export class CoreModule { }
