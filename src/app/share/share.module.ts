d7e009a919d5b9b6bb25318cac474298a92fb6dd
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-10-22 21:27:32 +0100

    feat(Button): Create app-button.

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TitleComponent } from './components/title/title.component';
 import { SubtitleComponent } from './components/subtitle/subtitle.component';
 import { TagComponent } from './components/tag/tag.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TitleComponent, SubtitleComponent, TagComponent],
-  exports: [TitleComponent, SubtitleComponent, TagComponent]
+  declarations: [TitleComponent, SubtitleComponent, TagComponent, ButtonComponent],
+  exports: [TitleComponent, SubtitleComponent, TagComponent, ButtonComponent]
 })
 export class ShareModule { }
