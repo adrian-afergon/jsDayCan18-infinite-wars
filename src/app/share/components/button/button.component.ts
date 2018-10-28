c96307888b332e99eca50b5dfb13df87540d2e4f
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-10-28 16:50:05 +0000

    feat(Button): Add output event.

--- src/app/share/components/button/button.component.ts
+++ src/app/share/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
-
   constructor() { }
 
+  @Output() onClick = new EventEmitter();
+
+
   ngOnInit() {
+  }
+
+  public onButtonClick(event) {
+    this.onClick.emit(event);
   }
 
 }
