dfbc8ddaee85b2b4172c859b312d616e213bb270
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-11-01 11:39:16 +0000

    feat(button): update the button for can disable it.

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,6 +8,7 @@ import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 export class ButtonComponent implements OnInit {
   constructor() { }
 
+  @Input() disabled: boolean;
   @Output() onClick = new EventEmitter();
 
 
