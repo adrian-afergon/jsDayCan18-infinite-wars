c4dbbf51012acd1f47875815efc59bafa1a6f4c0
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-11-01 13:45:35 +0000

    feat(modal): Create a modal component.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,13 @@ import { TitleComponent } from './components/title/title.component';
 import { SubtitleComponent } from './components/subtitle/subtitle.component';
 import { TagComponent } from './components/tag/tag.component';
 import { ButtonComponent } from './components/button/button.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TitleComponent, SubtitleComponent, TagComponent, ButtonComponent],
-  exports: [TitleComponent, SubtitleComponent, TagComponent, ButtonComponent]
+  declarations: [TitleComponent, SubtitleComponent, TagComponent, ButtonComponent, ModalComponent],
+  exports: [TitleComponent, SubtitleComponent, TagComponent, ButtonComponent, ModalComponent]
 })
 export class SharedModule { }
