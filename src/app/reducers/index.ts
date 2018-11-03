78b4f776452b4e9243427bc930129776dee0be10
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-11-03 19:38:39 +0000

    feat(State): Create the basic state for Gauntlet.

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,14 +1,17 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
-import { environment } from '../../environments/environment';
+import { ActionReducerMap } from '@ngrx/store';
+import {Stone} from '../core/model/stones';
+
+interface GauntletState {
+  powerStone: Stone;
+  spaceStone: Stone;
+  realityStone: Stone;
+  mindStone: Stone;
+  soulStone: Stone;
+  timeStone: Stone;
+}
 
 export interface State {
-
+  gauntlet: GauntletState;
 }
 
 export const reducers: ActionReducerMap<State> = {
