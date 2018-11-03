120d961bb3f70682d724acb7c8679146155f8686
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-11-03 19:01:50 +0000

    feat(Container): Add unsubscribe when onDestroy container.

--- src/app/gauntlet/containers/gauntlet/gauntlet.component.ts
+++ src/app/gauntlet/containers/gauntlet/gauntlet.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { SocketService } from '../../../core/services/socket.service';
 import { StonesService } from '../../../core/services/stones.service';
 import { StoneModel } from '../../viewmodel/Stone.model';
 import { GauntletMapper } from '../../mappers/gauntlet.mapper';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-gauntlet',
   templateUrl: './gauntlet.component.html',
   styleUrls: ['./gauntlet.component.scss']
 })
-export class GauntletComponent implements OnInit {
+export class GauntletComponent implements OnInit, OnDestroy {
   public stones: StoneModel[];
-  constructor(private socketService: SocketService, private stonesService: StonesService) { }
+  public subscription: Subscription;
+  constructor(private socketService: SocketService, private stonesService: StonesService) {
+    this.stones = [];
+  }
 
   ngOnInit() {
-    this.stonesService.getStones().subscribe(gauntlet => this.stones = GauntletMapper.toViewModel(gauntlet));
+    this.subscription = this.stonesService.getStones().subscribe(gauntlet => this.stones = GauntletMapper.toViewModel(gauntlet));
   }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+}
 
   public isGauntletCompleted() {
     return !!this.stones.find(stone => !stone.equipped);
--- src/app/population/containers/population/population.component.ts
+++ src/app/population/containers/population/population.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SocketService} from '../../../core/services/socket.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-population',
   templateUrl: './population.component.html',
   styleUrls: ['./population.component.scss']
 })
-export class PopulationComponent implements OnInit {
+export class PopulationComponent implements OnInit, OnDestroy {
 
   population: number;
-  constructor(socketService: SocketService) {
-    socketService.onPopulation().subscribe( population => this.population = population);
+  subscription: Subscription;
+  constructor(private socketService: SocketService) {
   }
 
   ngOnInit() {
+    this.subscription = this.socketService.onPopulation().subscribe( population => this.population = population);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
