3ded2a5793760e79f1006fa9fc9dcc859fe67a63
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-11-03 20:26:25 +0000

    feat(Component): Connect component to State.

--- src/app/gauntlet/containers/gauntlet/gauntlet.component.ts
+++ src/app/gauntlet/containers/gauntlet/gauntlet.component.ts
@@ -3,7 +3,9 @@ import { SocketClient } from '../../../core/services/socket.client';
 import { StonesRepository } from '../../../core/services/stones.repository';
 import { StoneModel } from '../../viewmodel/Stone.model';
 import { GauntletMapper } from '../../mappers/gauntlet.mapper';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
+import {select, Store} from '@ngrx/store';
+import {GauntletState} from '../../../reducers/gauntlet.reducer';
 
 @Component({
   selector: 'app-gauntlet',
@@ -12,13 +14,15 @@ import {Subscription} from 'rxjs';
 })
 export class GauntletComponent implements OnInit, OnDestroy {
   public stones: StoneModel[];
+  public stones$: Observable<GauntletState>;
   public subscription: Subscription;
-  constructor(private socketClient: SocketClient, private stonesRepository: StonesRepository) {
+  constructor(private store: Store<GauntletState>, private socketClient: SocketClient, private stonesRepository: StonesRepository) {
     this.stones = [];
+    this.stones$ = store.pipe(select('gauntlet'));
   }
 
   ngOnInit() {
-    this.subscription = this.stonesRepository.getStones().subscribe(gauntlet => this.stones = GauntletMapper.toViewModel(gauntlet));
+    // this.subscription = this.stonesRepository.getStones().subscribe(gauntlet => this.stones = GauntletMapper.toViewModel(gauntlet));
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
@@ -32,5 +36,9 @@ export class GauntletComponent implements OnInit, OnDestroy {
   }
   public equipStone(stoneId) {
     this.stones.map( stone => stone.id === stoneId ? stone.equipped = !stone.equipped : stone);
+  }
+  public toViewModel(data) {
+    console.log('re-render...');
+    return GauntletMapper.toViewModel(data);
   }
 }
