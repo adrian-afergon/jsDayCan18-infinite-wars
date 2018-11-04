aa7d2923b0647910a612959099570cae83906e65
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-11-04 12:02:37 +0000

    feat(Action): Create actions types.

--- src/app/actions/gauntlet.actions.ts
+++ src/app/actions/gauntlet.actions.ts
@@ -1,7 +1,14 @@
 import {Action} from '@ngrx/store';
 
 export const EQUIP = '[Gauntlet] equip';
+
+export const GET_STONES = '[Gauntlet] get stones';
+export const GET_STONES_FULFILLED = '[Gauntlet] get stones fulfilled';
+export const GET_STONES_REJECTED = '[Gauntlet] get stones rejected';
+
 export const SNAP = '[Gauntlet] snap';
+export const SNAP_SUCESS = '[Gauntlet] snap success';
+export const SNAP_FAIL = '[Gauntlet] snap fail';
 
 export class Equip implements Action {
   readonly type = EQUIP;
