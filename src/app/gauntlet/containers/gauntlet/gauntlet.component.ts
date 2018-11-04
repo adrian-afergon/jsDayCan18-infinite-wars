e327d77602aaec2c3bd41d7070f0ff6349440f66
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-11-04 11:55:05 +0000

    feat(Action): Connect component to reducer.

--- src/app/gauntlet/containers/gauntlet/gauntlet.component.ts
+++ src/app/gauntlet/containers/gauntlet/gauntlet.component.ts
@@ -6,6 +6,7 @@ import {Observable} from 'rxjs';
 import {select, Store} from '@ngrx/store';
 import {GauntletState} from '../../../reducers/gauntlet.reducer';
 import {GauntletSelectors} from '../../../selectors/gauntlet.selectors';
+import {Equip} from '../../../actions/gauntlet.actions';
 
 @Component({
   selector: 'app-gauntlet',
@@ -13,11 +14,9 @@ import {GauntletSelectors} from '../../../selectors/gauntlet.selectors';
   styleUrls: ['./gauntlet.component.scss']
 })
 export class GauntletComponent implements OnInit {
-  public stones: StoneModel[];
   public stones$: Observable<Array<StoneModel>>;
   public isGauntletCompleted$: Observable<boolean>;
   constructor(private store: Store<GauntletState>, private socketClient: SocketClient, private stonesRepository: StonesRepository) {
-    this.stones = [];
     this.stones$ = store.pipe(select(GauntletSelectors.getGauntletAsViewModel));
     this.isGauntletCompleted$ = store.pipe(select(GauntletSelectors.isGauntletCompleted));
   }
@@ -30,6 +29,6 @@ export class GauntletComponent implements OnInit {
     this.socketClient.snap();
   }
   public equipStone(stoneId) {
-    this.stones.map( stone => stone.id === stoneId ? stone.equipped = !stone.equipped : stone);
+    this.store.dispatch(new Equip(stoneId));
   }
 }
