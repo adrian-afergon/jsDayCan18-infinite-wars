9c705e27d55dd1a03d7019631330e72a52538cd2
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-11-04 12:35:21 +0000

    feat(Gauntlet): isolate container from service.

--- src/app/gauntlet/containers/gauntlet/gauntlet.component.ts
+++ src/app/gauntlet/containers/gauntlet/gauntlet.component.ts
@@ -1,12 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import { SocketClient } from '../../../core/services/socket.client';
-import { StonesRepository } from '../../../core/services/stones.repository';
 import { StoneModel } from '../../viewmodel/Stone.model';
 import {Observable} from 'rxjs';
 import {select, Store} from '@ngrx/store';
 import {GauntletState} from '../../../reducers/gauntlet.reducer';
 import {GauntletSelectors} from '../../../selectors/gauntlet.selectors';
-import {Equip, GetStones} from '../../../actions/gauntlet.actions';
+import {Equip, GetStones, Snap} from '../../../actions/gauntlet.actions';
 
 @Component({
   selector: 'app-gauntlet',
@@ -16,7 +14,7 @@ import {Equip, GetStones} from '../../../actions/gauntlet.actions';
 export class GauntletComponent implements OnInit {
   public stones$: Observable<Array<StoneModel>>;
   public isGauntletCompleted$: Observable<boolean>;
-  constructor(private store: Store<GauntletState>, private socketClient: SocketClient) {
+  constructor(private store: Store<GauntletState>) {
     this.stones$ = store.pipe(select(GauntletSelectors.getGauntletAsViewModel));
     this.isGauntletCompleted$ = store.pipe(select(GauntletSelectors.isGauntletCompleted));
   }
@@ -26,7 +24,7 @@ export class GauntletComponent implements OnInit {
   }
 
   public onSnap() {
-    this.socketClient.snap();
+    this.store.dispatch(new Snap());
   }
   public equipStone(stoneId) {
     this.store.dispatch(new Equip(stoneId));
